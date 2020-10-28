7e11fcce15dfb1ad6a766d3ea0ab7c95b1513545
hinata398 <r.hinata0398@gmail.com>
2020-10-28 13:24:25 +0900

    submit q2

--- problems/medium/2-add-two-numbers/main.go
+++ problems/medium/2-add-two-numbers/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/big"
 )
 
 // Definition for singly-linked list.
@@ -31,11 +30,37 @@ func CreateListNode(l []int) *ListNode {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	l := CreateListNode([]int{1, 0, 1})
-	//PrintListNode(l)
-	return l
-}
+	ln := &ListNode{-1, nil}
+	c := ln
+	c1 := l1
+	c2 := l2
+	carry := 0
+	for c1 != nil || c2 != nil || carry != 0 {
+		v1 := 0
+		if c1 != nil {
+			v1 = c1.Val
+		}
+		v2 := 0
+		if c2 != nil {
+			v2 = c2.Val
+		}
+		v := v1 + v2 + carry
+		if v >= 10 {
+			v %= 10
+			carry = 1
+		} else {
+			carry = 0
+		}
+		n := &ListNode{v, nil}
+		c.Next = n
+		c = c.Next
+		if c1 != nil {
+			c1 = c1.Next
 
-func addBigInt() {
-	new(big.Int).Add()
+		}
+		if c2 != nil {
+			c2 = c2.Next
+		}
+	}
+	return ln.Next
 }
--- problems/medium/2-add-two-numbers/main_test.go
+++ problems/medium/2-add-two-numbers/main_test.go
@@ -3,11 +3,30 @@ package main
 import "testing"
 
 func TestAddTwoNumbers(t *testing.T) {
-	l1 := CreateListNode([]int{2, 4, 3})
-	l2 := CreateListNode([]int{5, 6, 4})
-	PrintListNode(l1)
-	PrintListNode(l2)
-	addTwoNumbers(l1, l2)
+	t.Run("Example 1", func(t *testing.T) {
+		l1 := CreateListNode([]int{2, 4, 3})
+		l2 := CreateListNode([]int{5, 6, 4})
+		PrintListNode(l1)
+		PrintListNode(l2)
+		l := addTwoNumbers(l1, l2)
+		PrintListNode(l)
+	})
+	t.Run("Example 2", func(t *testing.T) {
+		l1 := CreateListNode([]int{0})
+		l2 := CreateListNode([]int{0})
+		PrintListNode(l1)
+		PrintListNode(l2)
+		l := addTwoNumbers(l1, l2)
+		PrintListNode(l)
+	})
+	t.Run("Example 3", func(t *testing.T) {
+		l1 := CreateListNode([]int{9, 9, 9, 9, 9, 9, 9})
+		l2 := CreateListNode([]int{9, 9, 9, 9})
+		PrintListNode(l1)
+		PrintListNode(l2)
+		l := addTwoNumbers(l1, l2)
+		PrintListNode(l)
+	})
 }
 
 func BenchmarkAddTwoNumbers(b *testing.B) {
