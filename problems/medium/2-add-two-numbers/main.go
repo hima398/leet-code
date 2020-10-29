31160884aba673b4cb4ecab78f208f8e6d20abd8
hinata398 <r.hinata0398@gmail.com>
2020-10-29 13:17:30 +0900

    modify q2

--- problems/medium/2-add-two-numbers/main.go
+++ problems/medium/2-add-two-numbers/main.go
@@ -30,37 +30,29 @@ func CreateListNode(l []int) *ListNode {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	ln := &ListNode{-1, nil}
+	ln := &ListNode{}
 	c := ln
 	c1 := l1
 	c2 := l2
 	carry := 0
 	for c1 != nil || c2 != nil || carry != 0 {
-		v1 := 0
+		v := carry
 		if c1 != nil {
-			v1 = c1.Val
+			v += c1.Val
+			c1 = c1.Next
 		}
-		v2 := 0
 		if c2 != nil {
-			v2 = c2.Val
+			v += c2.Val
+			c2 = c2.Next
 		}
-		v := v1 + v2 + carry
 		if v >= 10 {
 			v %= 10
 			carry = 1
 		} else {
 			carry = 0
 		}
-		n := &ListNode{v, nil}
-		c.Next = n
+		c.Next = &ListNode{v, nil}
 		c = c.Next
-		if c1 != nil {
-			c1 = c1.Next
-
-		}
-		if c2 != nil {
-			c2 = c2.Next
-		}
 	}
 	return ln.Next
 }
